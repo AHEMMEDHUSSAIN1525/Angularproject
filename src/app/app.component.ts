cfff5f7f0965e40a4e9ad2f13d91681c7e2426cc
Ahemmed <chinturockzz0494@gmail.com>
2020-04-13 19:16:38 +0530

    Movies files added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'movie-app';
+ title = 'movie-app';
+
+ lists=[
+  
+   {
+    Label:"Tollywood",
+    Movie:" Mirchi ",
+    Hero:"Prabhas"
+    },
+
+    {
+      Label:"Bollywood",
+      Movie:"War",
+      Hero:"Hritik Roshan"
+    },
+
+    {
+        Label:"Hollywood",
+        Movie:"Terminator",
+        Hero:"Arnold"
+    },
+
+    {
+          Label:"Kollywood",
+          Movie:"KGF",
+          Hero:"Yash"
+    },
+   
+];
+additem(newitemLabel){
+  var newlist={
+    Label:"Industry",
+    Movie:newitemLabel,
+    Hero:" "
+  }
+  this.lists.push(newlist);
+  };
+  deleteitem(table){
+    this.lists=this.lists.filter(t=>t.Label!==table.Label);
+  }
 }
